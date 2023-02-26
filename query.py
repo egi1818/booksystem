b2a7ae4e922aa20472a3cfc83bccd1abdb028ed1
egi1818 <93832575+egi1818@users.noreply.github.com>
2023-02-26 13:06:50 +0000

    Update query.py

--- query.py
+++ query.py
@@ -7,11 +7,11 @@ def runQuery(query_str,query_type):
     result = []
     try:
         connection = psycopg2.connect(
-            user="lnunmydamiypgu",
-            password="c15d7dfc4fa15bb17ab4411cff3adac1e012d86a4fdf2fa4e2e01d769b1c98de",
-            host="ec2-35-168-122-84.compute-1.amazonaws.com",
+            user="lfdtytgdqrhbqu",
+            password="97b0e3a555d7ae30f730c48dd944cafd04f6c7338361bd86ee5d64cc17269969",
+            host="ec2-54-165-184-219.compute-1.amazonaws.com",
             port="5432",
-            database="d8slr01vp3bhig")
+            database="dda1fmsu59fshs")
         cursor = connection.cursor()
 
         cursor.execute(query_str)
